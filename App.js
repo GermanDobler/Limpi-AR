89acd351d9c57c2c89c539c5697614540974a20b
Enzoepet20 <enzobeltran.epet20.5to1ra.b@gmail.com>
2022-06-20 23:49:46 -0300

    comentarios borrados

--- App.js
+++ App.js
@@ -2,18 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginForm from './src/UI/components/LoginForm';
 
-//Acá conecto mis pantallas
-
-//El saludar es estático ahora
-//      <Saludar/>
-//      <Saludar/>
-//      <Saludar/>
-
-//Esto es un props
-//<View style={styles.container}>
-
-
-
 export default function App() {
   return (
       <LoginForm style={styles.contenedor_form} />
