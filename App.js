e19d37bcd0117f6a4f1b7917f34cb40eddff58b0
Enzoepet20 <enzobeltran.epet20.5to1ra.b@gmail.com>
2022-06-21 00:18:31 -0300

    cambios momentaneos

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginForm from './src/UI/components/LoginForm';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-      <LoginForm style={styles.contenedor_form} />
-  )
+    <div>Mis componentes:
+    <Micomponente eltexto="Tocar boton" subtitle="fdasfasdf"/>
+    <Micomponente eltexto="Hola BOTON" subtitle="-------" />
+    <Micomponente eltexto="messi"/>
+    <Micomponente eltexto="maradona"/>
+    </div>
+  );
+}
+//const App = () => <div>Este es mi componente<Micomponente/></div> tambien se puede mostrar asi
+class Micomponente extends React.Component {
+  /*state ={
+    show: true
+  }*/
+  /*render(){
+    if (this.state.show){
+      return (
+        <div id="hola">
+          <h3 id="pelotudear">{this.props.subtitle}</h3>
+          {this.props.eltexto}
+          <button onClick={() => this.setState({show: false})} id="boton">hola</button>
+        </div>
+        ) 
+    } else {
+      return <h1 class="h1hola">No hay elementos</h1>
+    }
+  }*/
 }
 
-const styles = StyleSheet.create({
-  contenedor_form: {
-    flex: 1,
-    backgroundColor: '#FF00FF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
