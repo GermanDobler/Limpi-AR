9f37515c6a91aec752a0d47b8c3c3d6ce629cd47
Enzoepet20 <enzobeltran.epet20.5to1ra.b@gmail.com>
2022-06-22 10:39:21 -0300

    fix app.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View } from 'react-native-web';
 import ImageDisplay from './src/UI/components/ImageDisplay';
+import PrincipalScreen from './src/UI/components/PrincipalScreen';
+import Sector from './src/UI/components/Sector';
 //import LoginForm from './src/UI/components/LoginForm';
 
 function App() {
