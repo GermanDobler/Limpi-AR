f1be31c3d3aa9ba8091b628c80b4fb4e4b8817fb
Enzoepet20 <enzobeltran.epet20.5to1ra.b@gmail.com>
2022-06-25 19:58:40 -0300

    FIx: AsignarSector.js y SectorDisplay.js

--- src/UI/components/AsignarSector.js
+++ src/UI/components/AsignarSector.js
@@ -1,6 +1,6 @@
-import react, { useState } from "react";
-import { View } from "react-native-web";
-
+import react, { useState, Fragment } from "react";
+import { View, } from "react-native-web";
+import SvgFlechaizq from "./SvgFlechaizq";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/ImageDisplay.css';
 import SectorDisplay from "./SectorDisplay";
@@ -8,22 +8,62 @@ import { Row, Col } from 'react-bootstrap'
 
 export default function AsignarSector(props) {
     return (
+
         <div className="fondo">
+            
             <View>
+            <SvgFlechaizq></SvgFlechaizq>
                 <Row>
                     <Col>
-                        <SectorDisplay nombre_Sector="Sector 1 " />
-                        <br /><br />
-                        <SectorDisplay nombre_Sector="Sector 2 " />
-                        <br /><br />
-                        <SectorDisplay nombre_Sector="Sector 3 " />
-                        <br /><br />
-                        <SectorDisplay nombre_Sector="Sector 4 " />
-                        <br /><br />
-                        <SectorDisplay nombre_Sector="Sector 5 (Opcional)" />
-                        <br /><br /><br />
+                        <h1 className="centrar"> AsignarSector</h1>
                     </Col>
                 </Row>
+                <br />
+                <Row>
+                    <Col>
+                        <Row>
+                            <Col sm="5">
+                            </Col>
+                            <Col sm="5">
+                                <SectorDisplay nombre_Sector="Sector 1 " sm="5" />
+                                <br />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm="5">
+                            </Col>
+                            <Col sm="5" >
+                            <SectorDisplay nombre_Sector="Sector 2 "  />
+                            <br />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm="5">
+                            </Col>
+                            <Col sm="5" >
+                            <SectorDisplay nombre_Sector="Sector 3 "  />
+                            <br />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm="5">
+                            </Col>
+                            <Col sm="5" >
+                            <SectorDisplay nombre_Sector="Sector 4 "  />
+                            <br />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm="5">
+                            </Col>
+                            <Col sm="5" >
+                            <SectorDisplay nombre_Sector="Sector 5 (Opcional) "  />
+                            <br />
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <br /><br />
             </View>
         </div>
     )
