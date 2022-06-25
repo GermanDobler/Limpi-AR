d0584876afbc3bf66436ba484acfeb5ca242f678
Enzoepet20 <enzobeltran.epet20.5to1ra.b@gmail.com>
2022-06-25 18:52:18 -0300

    FIX: AsignarSector.js y ImageDisplay correcion de filas y columnas

--- src/UI/components/AsignarSector.js
+++ src/UI/components/AsignarSector.js
@@ -1,33 +1,30 @@
 import react, { useState } from "react";
 import { View } from "react-native-web";
-import './styles/ImageDisplay.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/ImageDisplay.css';
 import SectorDisplay from "./SectorDisplay";
 import { Row, Col } from 'react-bootstrap'
 
 export default function AsignarSector(props) {
     return (
-        <div className="background">
-         <View>
-         <Row>
-         <Col>
-         <SectorDisplay nombre_Sector="Sector 1 "/>
-         </Col>
-         <Col>
-         <SectorDisplay nombre_Sector="Sector 2 "/>
-         </Col>
-         <Col>
-         <SectorDisplay nombre_Sector="Sector 3 "/>
-         </Col>
-         <Col>
-         <SectorDisplay nombre_Sector="Sector 4 "/>
-         </Col>
-         <Col>
-         <SectorDisplay nombre_Sector="Sector 5 (Opcional)" />
-         </Col>
-         </Row>
-
-         </View>
-            </div>
+        <div className="fondo">
+            <View>
+                <Row>
+                    <Col>
+                        <SectorDisplay nombre_Sector="Sector 1 " />
+                        <br /><br />
+                        <SectorDisplay nombre_Sector="Sector 2 " />
+                        <br /><br />
+                        <SectorDisplay nombre_Sector="Sector 3 " />
+                        <br /><br />
+                        <SectorDisplay nombre_Sector="Sector 4 " />
+                        <br /><br />
+                        <SectorDisplay nombre_Sector="Sector 5 (Opcional)" />
+                        <br /><br /><br />
+                    </Col>
+                </Row>
+            </View>
+        </div>
     )
 }
