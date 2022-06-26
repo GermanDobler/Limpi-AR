52fcdd8c4bf175672d8273fa6d5d67004dfd7eac
Jeremias verge <jeremiasverge94@gmail.com>
2022-06-26 18:38:08 -0300

    Feat: PersonalInfo.js *Eliminamos CancelSubmit

---
+++ src/UI/components/PersonalInfo.js
@@ -0,0 +1,23 @@
+import Guardar from "./Guardar";
+import Cancel from "./Cancel";
+import Row from "react-bootstrap/Row";
+import { Fragment } from "react";
+import Col from "react-bootstrap/Col";
+export default function PersonalInfo() {
+    return (
+        <Fragment>
+          <Row className="Form">
+            <Col className="container_nombre">
+<h2></h2>
+            </Col>
+            <Col className="container_nombre">
+
+</Col>
+            </Row>  
+
+
+
+
+        </Fragment>
+    )
+}
