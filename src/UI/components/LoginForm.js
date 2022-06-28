ff3ac2444c0e6cf6d48dea800a2bf27673c9db70
GermanDobler <german.crack@hotmail.com>
2022-06-28 20:51:53 -0300

    Fix: LoginForm *Se utilizo libreria MUI para realizarlo

--- src/UI/components/LoginForm.js
+++ src/UI/components/LoginForm.js
@@ -1,41 +1,105 @@
-//Siempre deben de importar la estructura de react
-import react, { Fragment } from "react";
-import { Col, Row } from "react-bootstrap";
-//Este import genera la transición a los react
-import { Button } from "@material-ui/core";
-import { TextInput} from "react-native";
-import './styles/LoginForm.css';
-export default function LoginForm() {
-    return (
-        <Fragment>
-            <div className="fondo">
-                <Row>
-                </Row>
-            
-                <img src={require('./logo.png')} className="img" />
-                <Row className="contenedor-row">
-                    <Row className="contenedor">
-                        <TextInput placeholder="DNI -Sin Puntos-" className="input" id="dni" />
-                    </Row>
-                    <Row className="contenedor">
-                        <TextInput placeholder="Contraseña" className="input" id="contrasenia" />
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Button className="btn_signin" variant="contained">
-                                <span>Iniciar Sesión</span>
-                            </Button>
-                        </Col>
-                        <Col>
-                        <Button className="btn_register" variant="contained">
-                                <span>Registrarse</span>
-                            </Button>
-                        </Col>
-                    </Row>
-                </Row>
-            </div>
-        </Fragment>
-    )
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        Limpi-AR
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
+const theme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      dni: data.get('dni'),
+      contrasenia: data.get('contrasenia'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box className='box'>
+        <img src={require('./logo.png')} className="img" />
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            {/* <LockOutlinedIcon /> */}
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Inicio de Sesión
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="dni"
+              label="DNI"
+              name="DNI"
+              autoComplete="DNI"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="Contraseña"
+              label="Contraseña"
+              type="Contraseña"
+              id="contrasenia"
+              autoComplete="current-contraseña"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Recordar"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Iniciar Sesión
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Olvidaste tu contraseña?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Nuevo usuario? Registarse"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }}></Copyright>
+      </Container>
+    </ThemeProvider>
+  );
+}
 
