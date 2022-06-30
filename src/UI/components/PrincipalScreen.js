ca5a0c9e60e2b394c77862c4491dcbacf99d46e5
GermanDobler <german.crack@hotmail.com>
2022-06-30 17:50:53 -0300

    Fix: PrincipalScreen *Nuevo diseño de la pantalla *Implementacion de la libreria MUI

--- src/UI/components/PrincipalScreen.js
+++ src/UI/components/PrincipalScreen.js
@@ -2,6 +2,7 @@ import react, { Fragment, useState } from "react";
 import { View } from "react-native-web";
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/PrincipalScreen.css'
+import NavBar from './NavBar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Sector from "./Sector";
@@ -9,49 +10,27 @@ import Cancel from "./Cancel";
 import Guardar from "./Guardar";
 import SvgFlechaizq from "./SvgFlechaizq";
 import SvgPerfil from "./SvgPerfil"
+import { Container } from "react-bootstrap";
+import { Box } from "@material-ui/core";
 //import Col from 'react-bootstrap/Col'
 export default function PrincipalScreen(props) {
     const sectores = [
-        { id:1, nombre: 'Direccion', estado: 'limpio'},
+        { id:1, nombre: 'Dirección', estado: 'limpio'},
         { id:2, nombre: 'Taller - Soldadura', estado: 'sucio'},
-        { id:3, nombre: 'Pallillo', estado: 'sucio'},
+        { id:3, nombre: 'Taller - Electricidad', estado: 'sucio'},
         { id:4, nombre: 'Taller - Carpinteria', estado: 'sucio'},
         { id:5, nombre: 'Aula', estado: 'limpio'},
+        { id:6, nombre: 'Aula', estado: 'limpio'},
     ];
+
     return (
         <Fragment>
-            <div className="fondo">
-                <Row className="header">
-                    <Col sm="2">
-                        <SvgFlechaizq />
-                    </Col>
-                    <Col sm="8">
-                    </Col>
-                    <Col sm="2" className="verde">
-                        <SvgPerfil></SvgPerfil>
-                    </Col>
-                </Row>
-                <Row className="seccion">
-                    <Col >
-                        Secciones
-                    </Col>
-                </Row>
-                {/* props(estado=(sucio/limpio)) */}
-                <Row className="container-sector">
-                    {sectores.map(sector =>(
-                        <Sector key={sector.id} nombre={sector.nombre} estado={sector.estado}></Sector>
-                    )) }
-                </Row>
-                <Row className="container-sector">
-                    <Col sm="4">
-                        <Cancel eliminar="Cancelar"></Cancel>
-                    </Col>
-                    <Col sm="4"></Col>
-                    <Col sm="4">
-                        <Guardar guardar="Guardar Cambios"></Guardar>
-                    </Col>
-                </Row> 
-            </div>
+            <NavBar></NavBar>
+            <Container>
+            {sectores.map(sector =>(
+                <Sector key={sector.id} nombre={sector.nombre} estado={sector.estado} sx={{mb:4}}></Sector>
+            ))}
+            </Container>
         </Fragment>
     );
 }
--- src/UI/components/Sector.js
+++ src/UI/components/Sector.js
@@ -1,47 +1,86 @@
-import { StyleSheet, Button, View } from 'react-native'
-import React, { Fragment, useState } from 'react'
-import { Row, Col, Accordion } from 'react-bootstrap'
-import './styles/Sector.css'
-import Flecha from './SvgFlecha'
-import { render } from 'react-dom'
-import Guardar from './Guardar'
-
-export default function Sector(props) {
-
-        return (
-            <Fragment>
-                <div className="content">
-                    <Accordion defaultActiveKey={0} >
-                        <Accordion.Item eventKey='0' className={props.estado}>
-                            <Accordion.Header>
-                            <Row className="sector">
-                                {props.nombre}
-                            </Row>
-                            </Accordion.Header>
-                            <Accordion.Body>
-                                <Row>
-                                    <Col>
-                                    </Col>
-                                    <Col>
-                                    </Col>
-                                    <Col>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col>
-                                    </Col>
-                                    <Col>
-                                    </Col>
-                                    <Col>
-                                    
-                                    <Guardar guardar="Cambiar estado"></Guardar>
-                                    </Col>
-                                </Row>
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
-                </div>
-            </Fragment>
-        )
-    }
-
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import './styles/sector.css'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import { FormControlLabel, Switch } from '@mui/material';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+const modal_box = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+}
+export default function SimpleAccordion(props) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    return (
+        <div>
+            <Accordion sx={{ mb: 8, }}>
+                <AccordionSummary
+                    aria-controls="panel1a-header"
+                    id="panel1a-header"
+                    className={props.estado}
+                    sx={{ mt: 2, }}
+                >
+                    <Typography className='sector_nombre'>{props.nombre}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        <Box sx={{ flexGrow: 1, mt: 3 }}>
+                            <Grid container spacing={1} >
+                                <Grid xs={3}>
+                                </Grid>
+                                <Grid xs={9}>
+                                    Detalles:{props.detalles}
+                                </Grid>
+                                <Grid xs={3}>
+                                    <Button onClick={handleOpen}>imagen</Button>
+                                    <Modal
+                                        open={open}
+                                        onClose={handleClose}
+                                        aria-labelledby="modal-modal-title"
+                                        aria-describedby="modal-modal-description"
+                                    >
+                                        <Box sx={modal_box}>
+                                            <Typography id="modal-modal-title" variant="h6" component="h2">
+                                                Sector: {props.nombre}
+                                            </Typography>
+                                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                                imagen del sector ??????
+                                            </Typography>
+                                        </Box>
+                                    </Modal>
+                                </Grid>
+                                <Grid xs={9}>
+                                    Ultima Limpieza: --/--/----
+                                </Grid>
+                                <Grid xs={9}>
+                                </Grid>
+                                <Grid xs={3}>
+                                    <FormControlLabel
+                                        value="start"
+                                        control={<Switch color="primary" />}
+                                        label="Estado"
+                                        labelPlacement="start"
+                                    />
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
+}
