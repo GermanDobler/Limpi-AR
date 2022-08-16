0e26965d50c4a260bb734c55e9945947487137af
Enzoepet20 <cartaspoke2@gmail.com>
2022-08-16 14:42:23 -0300

    Fix: ScreenSector.jsx

--- screens/ScreenSector.jsx
+++ screens/ScreenSector.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { Button, View} from 'react-native';
 import SectorSelect from '../src/components/SectorSelect';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 export default function ScreenSector({ navigation, props }) {
   return (
     <View>
+      <ArrowBackIcon></ArrowBackIcon>
 <SectorSelect nombre_sector="Sector 1"></SectorSelect>
 <SectorSelect nombre_sector="Sector 2"></SectorSelect>
 <SectorSelect nombre_sector="Sector 3"></SectorSelect>
