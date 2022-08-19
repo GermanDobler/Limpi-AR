4eefdb009fe1bed3f377a319db43145547f1ab99
Enzoepet20 <cartaspoke2@gmail.com>
2022-08-18 23:47:24 -0300

    todo arreglado

--- App.js
+++ App.js
@@ -2,22 +2,16 @@ import React from 'react';
 import MainStack from './navigation/MainStack'
 import SectorSelect from './src/components/SectorSelect';
 import ScreenSector from './screens/ScreenSector';
-<<<<<<< HEAD
 import SelectorDatosPersonales from './src/components/SelectorDatosPersonales';
 import ScreenSecretaria from './screens/ScreenSecretaria';
-=======
 import PersonalInfo from './screens/PersonalInfo';
->>>>>>> f37f3e34e1545065902c379bd5130cd949761af3
 export default function App() {
   return (
     <div>
       {/* <MainStack/> */}
       {/* <ScreenSector/> */}
-<<<<<<< HEAD
       <ScreenSecretaria/>
-=======
-    <PersonalInfo></PersonalInfo>
->>>>>>> f37f3e34e1545065902c379bd5130cd949761af3
+    {/* <PersonalInfo></PersonalInfo> */}
     </div>
   );
 }
