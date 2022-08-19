f37f3e34e1545065902c379bd5130cd949761af3
Jeremias Verge <jeremiasverge94@gmail.com>
2022-08-18 23:38:48 -0300

    New: PersonalInfo *Areamos la pantala PersonalInfo *Añadimos un boton de guardar *Añadimos una flecha de retroceder

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import MainStack from './navigation/MainStack'
 import SectorSelect from './src/components/SectorSelect';
 import ScreenSector from './screens/ScreenSector';
+import PersonalInfo from './screens/PersonalInfo';
 export default function App() {
   return (
     <div>
       {/* <MainStack/> */}
-      <ScreenSector/>
+      {/* <ScreenSector/> */}
+    <PersonalInfo></PersonalInfo>
     </div>
   );
 }
