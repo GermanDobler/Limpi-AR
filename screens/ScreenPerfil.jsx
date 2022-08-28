149e556540f3e018e80e578c785c6d31f64d881a
Jeremias Verge <jeremiasverge94@gmail.com>
2022-08-27 23:30:03 -0300

    FIX: ARREGLOS EN PERFIL *arreglemos etiquetas que no usamos

--- screens/ScreenPerfil.jsx
+++ screens/ScreenPerfil.jsx
@@ -7,8 +7,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
