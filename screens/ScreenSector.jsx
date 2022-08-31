490c034ef7114c3154ad435c4a08650ade58314f
Enzoepet20 <cartaspoke2@gmail.com>
2022-08-30 22:46:07 -0300

    Feat: Styled Js añadido y estilos basicos

--- screens/ScreenSector.jsx
+++ screens/ScreenSector.jsx
@@ -1,20 +1,32 @@
 import * as React from 'react';
-import { Button, View} from 'react-native';
+import { Button, View } from 'react-native';
 import SectorSelect from '../src/UI/components/SectorSelect';
 import NavBar from '../src/UI/components/NavBar';
 import ButtonMui from '../src/UI/components/ButtonMui';
+import _JSXStyle from 'styled-jsx/style'
 export default function ScreenSector({ navigation, props }) {
   return (
     <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-    <NavBar navigation={navigation} paqueteP={"Pantalla Sector"}></NavBar>
-        <SectorSelect nombre_sector="Sector 1"></SectorSelect>
-        <SectorSelect nombre_sector="Sector 2"></SectorSelect>
-        <SectorSelect nombre_sector="Sector 3"></SectorSelect>
-        <SectorSelect nombre_sector="Sector 4"></SectorSelect>
-        <SectorSelect nombre_sector="Sector 5 (Opcional)"></SectorSelect>
-        <ButtonMui name="Guardar Sectores"></ButtonMui>
+      <NavBar navigation={navigation} paqueteP={"Pantalla Sector"}></NavBar>
+      <div className="fondo">
+        <br />
+      <SectorSelect nombre_sector="Sector 1"></SectorSelect>
+      <SectorSelect nombre_sector="Sector 2"></SectorSelect>
+      <SectorSelect nombre_sector="Sector 3"></SectorSelect>
+      <SectorSelect nombre_sector="Sector 4"></SectorSelect>
+      <SectorSelect nombre_sector="Sector 5 (Opcional)"></SectorSelect>
+      <ButtonMui name="Guardar Sectores" onClick={() => navigation.navigate('ScreenSecretaria')}></ButtonMui>
+      </div>
+      <style jsx>{`
+      body {
+        background: grey;
+      }
+      .fondo{
+        background: #EEE5E3;
+      }
+    `}</style>
     </View>
-    
+
   );
 }
 
