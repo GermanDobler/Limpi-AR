98d3cef2c1bdf457ea493a60d8e6ee1fc71d10f4
GermanDobler <german.crack@hotmail.com>
2022-09-02 12:06:08 -0300

    Feat: Switches Fix: Accordion Estilos

--- src/UI/components/Accordion.jsx
+++ src/UI/components/Accordion.jsx
@@ -6,8 +6,11 @@ import MuiAccordion from '@mui/material/Accordion';
 import MuiAccordionSummary from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import { Grid, popoverClasses } from '@mui/material';
+import { FormControlLabel, Grid, popoverClasses, Switch } from '@mui/material';
 import _JSXStyle from 'styled-jsx/style'
+import Slide from '@mui/material/Slide';
+import { Box } from '@mui/system';
+import CustomizedSwitches from './SwitchIOS';
 
 const Accordion = styled((props) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
@@ -48,12 +51,18 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 }));
 
 export default function CustomizedAccordions(props) {
+  const [checked, setChecked] = React.useState(false);//Para la animación de la img. if == false no la hace, if == true la hace.
+  const containerRef = React.useRef(null);
+  const handleChangee = () => {
+    setChecked((prev) => !prev);
+  };
   const [expanded, setExpanded] = React.useState(false);
   const [nombre, setNombre] = useState(props.sector.nombre);
   //   setId(props.sector.id);
   const handleChange = (panel) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
     setNombre(props.sector.nombre);
+    setChecked((prev) => !prev);
   };
 
   return (
@@ -70,22 +79,53 @@ export default function CustomizedAccordions(props) {
         padding:15px 15px 0 15px !important;
       }
     `}</style>
-    <Accordion className='Contendedor-Accordion' key={props.sector.id} expanded={expanded === nombre} onChange={handleChange(nombre)}>
-      <AccordionSummary aria-controls={nombre} id={nombre}>
-        <Typography>{props.sector.nombre}</Typography>
-      </AccordionSummary>
-      <AccordionDetails className='Contenedor-Detail'>
+      <Accordion className='Contendedor-Accordion' key={props.sector.id} expanded={expanded === nombre} onChange={handleChange(nombre)}>
+        <AccordionSummary aria-controls={nombre} id={nombre}>
+          <Typography>{props.sector.nombre}</Typography>
+        </AccordionSummary>
+        <AccordionDetails className='Contenedor-Detail'>
           <Grid container spacing={1}>
             <Grid item xs={4} className='Grid-Item'>
-            <img src={props.sector.img} height={180} width={150} />
+              <Box
+                sx={{
+                  
+                  display: 'flex',
+                  borderRadius: 1,
+                  bgcolor: (theme) =>
+                    theme.palette.mode === 'light' ? 'grey.100' : 'grey.900',
+                  overflow: 'hidden',
+                }}
+                className="Grid-Item"
+                ref={containerRef}
+              >
+                  <Slide direction="up" in={checked} container={containerRef.current} style={{ transitionDelay: checked ? '250ms' : '0ms' }}>
+                    <img src={props.sector.img} height={180} width={150} />
+                  </Slide> 
+              </Box>
             </Grid>
-            <Grid item xs={8} className='Grid-Item'>
-            {props.sector.detalles}
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat obcaecati pariatur dignissimos eum voluptatum, error provident voluptatem quis aspernatur quia, optio ab. Dolorum hic unde autem recusandae vero explicabo. Porro?
-            </Grid>          
+            <Grid item xs={8} container className='Grid-Item'>
+              <Grid item xs={12} className='Grid-Item'>
+                <Typography>
+                  Detalles:{props.sector.detalles}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime possimus, quos provident sequi temporibus delectus, eius fuga ad quae illum saepe, atque error reprehenderit quia molestiae unde corrupti. Autem, facilis!
+                </Typography>
+              </Grid>
+              <Grid item xs={12} className='Grid-Item'>
+                Ultima Limpieza: --/--/----
+              </Grid>
+              <Grid item xs={4} className='Grid-Item'>
+                
+              </Grid>
+              <Grid item xs={4} className='Grid-Item'>
+                
+              </Grid>
+              <Grid item xs={4} className='Grid-Item'>
+              <CustomizedSwitches/>
+              </Grid>
+            </Grid>
           </Grid>
-      </AccordionDetails>
-    </Accordion>
+        </AccordionDetails>
+      </Accordion>
     </>
   );
 }
